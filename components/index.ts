5fbf2147a7f05d86adb9384462fdb875084cdccf
leulenq <78839378+leulenq@users.noreply.github.com>
2025-09-25 02:38:20 -0400

    Create index.ts

---
+++ components/index.ts
@@ -0,0 +1,10 @@
+export { default as Hero } from './Hero';
+export { default as About } from './About';
+export { default as Testimonials } from './Testimonials';
+export { default as Services } from './Services';
+export { default as Gallery } from './Gallery';
+export { default as Contact } from './Contact';
+export { default as FAQs } from './FAQs';
+export { default as Pricing } from './Pricing';
+export { default as Map } from './Map';
+export { default as Features } from './Features';
