ed8b6150bf61d1e3c12ad39b3b030e0912d8ede5
leulenq <78839378+leulenq@users.noreply.github.com>
2025-10-01 16:15:43 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,2 @@
+"use client";
 import Image from 'next/image'; import { use client } from 'react'; export default function Page() { return ( <div> <Image src='/hero.jpg' alt='Hero Image' width={500} height={300} /> <h1>Welcome to our company</h1> <p>Let&apos;s talk!</p> </div> ); }
